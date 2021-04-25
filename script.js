aa2aa426ed33d5aac18417d2aefa282f436d0536
Swan Pham <accounting@nailsrus.ca>
2021-04-24 21:07:25 -0400

    add index.html

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-// Generate Password
-function generate(){
+// // Generate Password
+// function generate(){
 
-  // Set password legth/complexity
-  let complexity = document.getElementById("slider").value;
+//   // Set password legth/complexity
+//   let complexity = document.getElementById("slider").value;
 
-  // Possible password values
-  let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+//   // Possible password values
+//   let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
-  let password = "";
+//   let password = "";
 
-  // Create for loop to chosse password characters
-  for(var i = 0; i <= complexity; i++) {
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-  }
+//   // Create for loop to chosse password characters
+//   for(var i = 0; i <= complexity; i++) {
+//     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+//   }
 
-  // Add password to texbox/display area
-  document.getElementById("display").value = password;
+//   // Add password to texbox/display area
+//   document.getElementById("display").value = password;
 
-  // Add password to previously generated passwords section
-  document.getElementById("lastNums").innerHTML += password + "<br />";
+//   // Add password to previously generated passwords section
+//   document.getElementById("lastNums").innerHTML += password + "<br />";
 
-}
+// }
 
-// Set default length display of 25
-document.getElementById("length").innerHTML = "length: 25";
+// // Set default length display of 25
+// document.getElementById("length").innerHTML = "length: 25";
 
-// Function to set length based on slider position
-document.getElementById("slider").oninput = function() {
+// // Function to set length based on slider position
+// document.getElementById("slider").oninput = function() {
 
-  if(document.getElementById("slider").value > 0) {
-    document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-  }
-  else {
-    document.getElementById("length").innerHTML = "length: 1";
-  }
-}
+//   if(document.getElementById("slider").value > 0) {
+//     document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+//   }
+//   else {
+//     document.getElementById("length").innerHTML = "length: 1";
+//   }
+// }
 
-// Function to copy password to clipboard
-function copyPassword() {
 
-  document.getElementById("display").select();
 
-  document.execCommand("copy");
 
-  alert("password copied to clipboard!");
-}
+
+
